eab6e37c00ebae4959d166e410e772b2109b13f3
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-06-26 20:23:02 +0600

    Today's last Commit

--- script.js
+++ script.js
@@ -2,5 +2,12 @@ $(document).ready(function(){
     $(window).scroll(function(){
 
     });
-    
+
+
+    var type = Typed(".typing", {
+        strings: ["Web Developer", "Designer", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    })
 })
