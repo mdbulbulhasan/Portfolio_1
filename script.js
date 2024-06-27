2cbbc64c930ebb3ae172ba7a9657a29e7a4927d1
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-06-27 20:48:20 +0600

    Today's last update

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
-// Typing Feature Start
+
 $(document).ready(function(){
     $(window).scroll(function(){
 
-    });
-
-
-    var typed = new Typed (".typing", {
-        // .typing is class which is belong to html file
-        strings: ["Developer", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 80,
-        loop:true
-    });
+    });     
 })
-// Typing Feature End
+var typed = new Typed (".typing", {
+    // .typing is class which is belong to html file
+    strings: ["Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true
+});
+
+
+var typed = new Typed (".typing-2", {
+    // .typing is class which is belong to html file
+    strings: ["Developer", "Designer", "Freelancer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop:true
+});
