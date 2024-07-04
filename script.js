8d404d4e438265b644d8c7b48fd91bb03f0ba18f
Md Bulbul Hasan <bulbulhasan095@gmail.com>
2024-07-04 08:08:32 +0600

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ $(document).ready(function(){
 
     });     
 })
+
+// Home Section Start
 var typed = new Typed (".typing", {
     // .typing is class which is belong to html file
     strings: ["Web Developer", "Designer", "Freelancer"],
@@ -11,8 +13,9 @@ var typed = new Typed (".typing", {
     backSpeed: 60,
     loop:true
 });
+// Home Section End
 
-
+// About Section Start
 var typed = new Typed (".typing-2", {
     // .typing is class which is belong to html file
     strings: ["Web Developer", "Designer", "Freelancer"],
@@ -20,3 +23,35 @@ var typed = new Typed (".typing-2", {
     backSpeed: 60,
     loop:true
 });
+// About Section End
+
+// My Team Section Start
+
+// Add Slider Option
+$('.carousel').owlCarousel({
+    margin:20,
+    loop:true,
+    autoplayTimeout: 2000,
+    autoplayHoverPause: true,
+    responsive: {
+        0:{
+            items: 1,
+            nav: false
+        },
+        600:{
+            items:2,
+            nav: false
+        },
+        1000:{
+            items: 3,
+            nav: false
+        }
+    }
+});
+// Add Slider Option
+
+// My Team Section End
+
+
+
+
